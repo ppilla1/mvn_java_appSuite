f0fdd8b68115c1390efed271813b04452311a4c7
Prashant <informprashant@gmail.com>
2023-04-08 09:40:25 +0530

    Initialize explration unit for Project Reactor

---
+++ explore-project-reactor/src/test/java/io/explore/AppTest.java
@@ -0,0 +1,21 @@
+package io.explore;
+
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Log4j2
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        log.info("Hello Project Reactor...");
+        assertTrue( true );
+    }
+}
