bdb4d4bc6ac1a94a957590e077d2694186df599f
Prashant Pillai <prashantpillai108@gmail.com>
2023-06-28 06:58:37 +0530

    Created new Antlr exploration project

---
+++ explore-antlr/src/test/java/io/explore/AppTest.java
@@ -0,0 +1,23 @@
+package io.explore;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+@Log4j2
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        log.info("Rigourous Test :-)");
+        assertTrue( true );
+    }
+}
