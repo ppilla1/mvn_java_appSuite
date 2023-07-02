14e26868ee3cc16afefad4b63a89d1b18a40d9ab
Prashant Pillai <prashantpillai108@gmail.com>
2023-07-02 21:42:05 +0530

    Cosmetic update

--- explore-antlr/src/test/java/io/explore/AppTest.java
+++ explore-antlr/src/test/java/io/explore/AppTest.java
@@ -21,8 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AppTest {
 
     @Test
-    public void testParsingArrayListInit()
-    {
+    public void testParsingArrayListInit() {
         try {
 
             // Create CharStream that reads from file
